b33cb0d9a859d4b180432aaf4e4ce2638349638c
isakulaksiz <isakulaksiz@outlook.com>
2020-11-02 00:06:10 +0300

    feat: Add BookType graphql

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const { graphqlHTTP } = require('express-graphql');
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+
+}));
+
+app.listen(8080,()=> {
+    console.log("listening port...");
+});
