5acb988c79a66c33eff847198d59e61f8ee24458
isakulaksiz <isakulaksiz@outlook.com>
2020-11-02 22:00:52 +0300

    fix: type relations

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -4,9 +4,9 @@ const _ = require('lodash');
 const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt } = graphql;
 
 var books=[
-    {name: "The Little Prince", genre: "Fantasy", id: "1"},
-    {name: "The Final Empire", genre: "Fantasy", id: "2"},
-    {name: "The Long Earth", genre: "Sci-Fi", id: "3"}
+    {name: "The Little Prince", genre: "Fantasy", id: "1", authorId: "1"},
+    {name: "The Final Empire", genre: "Fantasy", id: "2", authorId: "2"},
+    {name: "The Long Earth", genre: "Sci-Fi", id: "3", authorId: "3"}
 ];
 
 var authors=[
@@ -20,7 +20,14 @@ const BookType = new GraphQLObjectType({
     fields:() => ({
         id: {type: GraphQLID},
         name: {type: GraphQLString},
-        genre: {type: GraphQLString}
+        genre: {type: GraphQLString},
+        author: {
+            type: AuthorType,
+            resolve(parent, args){
+                console.log(parent);
+                return _.find(authors, {id: parent.authorId});
+            }
+        }
     }) 
 });
 
