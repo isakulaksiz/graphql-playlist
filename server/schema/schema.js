746820faefb13053476a66676da0cfbfcbfd0a9c
isakulaksiz <isakulaksiz@outlook.com>
2020-11-02 16:53:11 +0300

    error fix

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,31 +1,31 @@
 const graphql = require("graphql");
-const_=require("lodash");   
+const _ = require('lodash');  
 
-const { GraphQlObjectType, GraphQlString, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
 var books=[
-    {name: "The Little Prince", genre: "Fantasy", id:'1'},
+    {name: "The Little Prince", genre: "Fantasy", id: "1"},
     {name: "The Final Empire", genre: "Fantasy", id: "2"},
-    {name: "The Long Earth", genre: "Sci-Fi", id: "3"},
+    {name: "The Long Earth", genre: "Sci-Fi", id: "3"}
 ];
 
-const BookType = new GraphQlObjectType({
+const BookType = new GraphQLObjectType({
     name: "Book",
     fields:() => ({
-        id: {type: GraphQlString},
-        name: {type: GraphQlString},
-        genre: {type: GraphQlString}
+        id: {type: GraphQLString},
+        name: {type: GraphQLString},
+        genre: {type: GraphQLString}
     }) 
 });
 
-const RootQuery = new GraphQlObjectType({
+const RootQuery = new GraphQLObjectType({
     name: "RootQueryType",
     fields:{
         book:{
             type: BookType,
-            args: {id: {type: GraphQlString}},
+            args: {id: {type: GraphQLString}},
             resolve(parent, args){
-                return _find(books, {id: args.id});
+                return _.find(books, {id: args.id});
             }
         }
     }
