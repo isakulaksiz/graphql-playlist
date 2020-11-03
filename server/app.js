7ff820fa5e038c41b6126995d60a6ced97d7f0f5
isakulaksiz <isakulaksiz@outlook.com>
2020-11-03 12:12:35 +0300

    connecting to mLab

--- server/app.js
+++ server/app.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const { graphqlHTTP } = require('express-graphql');
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect("mongodb+srv://shaun:test123@qql-ninja.43vf0.mongodb.net/shaun?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connection.once("open",() => {
+    console.log("connected to database");
+});
 
 app.use('/graphql', graphqlHTTP({
     schema,
