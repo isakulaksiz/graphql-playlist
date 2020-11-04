911a0131b7ceb36aa3de2e9d2c27732b32320278
isakulaksiz <isakulaksiz@outlook.com>
2020-11-04 11:54:23 +0300

    fix: resolve function

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -16,6 +16,7 @@ const BookType = new GraphQLObjectType({
             resolve(parent, args){
                 console.log(parent);
                 //return _.find(authors, {id: parent.authorId});
+                return Author.findById(parent.authorId);
             }
         }
     })
@@ -31,6 +32,7 @@ const AuthorType = new GraphQLObjectType({
             type: new GraphQLList(BookType),
             resolve(parent, args){
                 //return _.filter(books, {authorId: parent.id});
+                return Book.find({authorId: parent.id});
             }
         }
     })
@@ -45,6 +47,7 @@ const RootQuery = new GraphQLObjectType({
             resolve(parent, args){
                 console.log(typeof(args.id));
                 //return _.find(books, {id: args.id});
+                return Book.findById(args.id);
             }
         },
         author: {
@@ -52,18 +55,21 @@ const RootQuery = new GraphQLObjectType({
             args: {id: {type: GraphQLID}},
             resolve(parent, args){
                 //return _.find(authors, {id: args.id});
+                return Author.findById(args.id);
             }
         },
         books: {
             type: new GraphQLList(BookType),
             resolve(parent, args){
                 //return books;
+                return Book.find({});
             }
         },
         author: {
             type: new GraphQLList(AuthorType),
             resolve(parent, args){
                 //return authors;
+                return Author.find({});
             }
         }
     }
