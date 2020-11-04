10df0de69106deee7581420423bdd8bb3bb12635
isakulaksiz <isakulaksiz@outlook.com>
2020-11-04 15:07:18 +0300

    feat: booklist component added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React,{Component} from 'react';
+import BookList from "./components/BookList";
 
 class App extends Component{
   render(){
     return(
       <div id="main">
         <h1>Application Reading List</h1>
+        <BookList />
       </div>
     );
   }
