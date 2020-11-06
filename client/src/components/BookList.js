85cb07a5ccaf461d6dee187b4b9bd809f87e809e
isakulaksiz <isakulaksiz@outlook.com>
2020-11-06 20:15:25 +0300

    feat: book details component added

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,7 +1,8 @@
 import React,{Component} from 'react';
 import { graphql } from "react-apollo";
 import {getBooksQuery} from "../queries/queries";
-
+import BookDetails from './BookDetails';
+ 
 class BookList extends Component{
   displayBooks(){
     var data = this.props.data;
@@ -22,6 +23,7 @@ class BookList extends Component{
           <ul id="book-list">
               {this.displayBooks()}
           </ul>
+          <BookDetails />
       </div>
     );
   }
